40310709260c380a430b1887c7eec499aa15881b
Milica Bucko <milica.milica.b@gmail.com>
2017-05-18 22:48:55 +0200

    Prilikom odabira kompanije prikazuju se svi njeni partneri.

--- fakturisanje/src/main/resources/static/controllers/pregledPreduzecaController.js
+++ fakturisanje/src/main/resources/static/controllers/pregledPreduzecaController.js
@@ -1,5 +1,14 @@
 app.controller('pregledPreduzecaController',['$scope', '$location', '$mdDialog', 'companyService', '$timeout','businessPartnerService', function($scope, $location, $mdDialog, companyService, $timeout, businessPartnerService){
 
+	$scope.onSelectEvent = function() {
+		if ($scope.selected[0] === undefined) {
+			$scope.businessPartners = [];
+		}
+		else {
+			$scope.businessPartners = $scope.selected[0].businessPartners;
+		}
+	}
+	
 	$scope.options = {
 		//autoSelect: true,
 		boundaryLinks: true,
@@ -17,29 +26,7 @@ app.controller('pregledPreduzecaController',['$scope', '$location', '$mdDialog',
 	$scope.selected = [];
 	$scope.bpselected = [];
 	
-	
-	
-	$scope.ispisiNesto = function() {
-		if($scope.selected === undefined) {
-		
-		}
-		else {
-			$scope.businessPartners = $scope.selected["0"].businessPartners;
-		}
-		console.log($scope.selected[0]);
-		
-		/*if($scope.selected[0].id != "undefined"){
-			
-			businessPartnerService.getAllBusinessPartners($scope.selected[0].id).then(function(response){
-				
-				$scope.businessPartners = response.data;
-				
-			});
-			
-		}*/
-		
 
-	}
 	
 	companyService.getAllCompanies().then(function(response){
 		 
